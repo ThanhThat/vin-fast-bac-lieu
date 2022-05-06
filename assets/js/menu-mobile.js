0cc6522de9f5bfe0db16b80ea399a56777424e78
That Thach <thachthanhthat1995@gmail.com>
2022-05-07 05:33:30 +0700

    add animation menu mobile

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -2,6 +2,10 @@ const menuMobile = document.getElementById('menu-mobile');
 const headerMenu = document.getElementById('header-menu');
 let headerHieght = headerMenu.clientHeight;
 const menuItems = document.querySelectorAll('.nav > li > a[href*="#"]');
+const menuLis = document.querySelectorAll('.nav > li');
+const burger = document.querySelector('#menu-mobile-icon--burger');
+const menuIconClose = document.querySelector('#menu-mobile-icon--close');
+menuIconClose.classList.add('menu-mobile-icon--none');
 
 // open menu 
 menuMobile.onclick = function() {
@@ -19,3 +23,27 @@ for(let menuItem of menuItems) {
         headerMenu.style.height = null;
     });
 }
+
+// ẩn nút close menu khi click vào items
+for (i=1; i<menuItems.length; i++) {
+    menuItems[i].addEventListener('click', () => {
+        burger.classList.toggle('menu-mobile-icon--none');
+        menuIconClose.classList.toggle('menu-mobile-icon--none');
+    })
+}
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('menu-mobile-icon--none');
+    menuIconClose.classList.toggle('menu-mobile-icon--none');
+    for(let menuLi of menuLis) {
+        menuLi.style.animation = `slideMenu ease 0.3s forwards ${1/6}s`;
+    }
+})
+
+menuIconClose.addEventListener('click', () => {
+    burger.classList.toggle('menu-mobile-icon--none');
+    menuIconClose.classList.toggle('menu-mobile-icon--none');
+    for(let menuLi of menuLis) {
+        menuLi.style.animation = null;
+    }
+})
